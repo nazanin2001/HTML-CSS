d98249bebb701b5959f74ee444116e0b04ad9f8b
nazanin2001 <53350653+nazanin2001@users.noreply.github.com>
2019-08-30 17:36:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+let lan= {English: "Hello world", Persian: "Salam donya" , Greek: "ya sas kosmos"};
+console.log(lan)
+
+console.log("iam awsem")let x;
+console.log(x)
+//3.1
+x = 100;
+console.log("the value of my variable x will be:100")
+console.log(x)
+//4
+
+const y='pizza';
+console.log('this is my favorit food');
+console.log(y);
+//5
+//i ahave to write in this place for number 5
+//6
+myarray =[];
+console.log(myarray);
+console.log("My array");
+console.log(myarray);
+let  animal = ["horse", "giraffe", "whale", "lizard"];
+console.log(animal.push("butterfly"));
+console.log(animal);
+//7
+let myString = "this is a test"
+console.log(myString.length);
+console.log(myString );
+console.log(myString.length)
+// 8
+const h = 5;
+const name ="nazanin";
+const lastName ="Paymard";
+const  age =18;
+
+console.log(h);
+console.log(name);
+console.log(lastName);
+console.log(age);
+console.log(typeof h);
+console.log(typeof name);
+console.log(typeof lastName);
+console.log(typeof age);
+console.log(typeof true);
+
+(typeof h==typeof name) ? console.log('same type'):console.log('no');
+
+(typeof h==typeof lastName) ? console.log('same type'):console.log('no');
+
+(typeof h==typeof age) ? console.log('same type'):console.log('no');
+
+(typeof name==typeof gamer) ? console.log('same type'):console.log('no');
+
+(typeof name==typeof best) ? console.log('same type'):console.log('no');
+
+(typeof best==typeof gamer) ? console.log('same type'):console.log('no');
+// 9
+var i =7;
+console.log(i%3);
+console.log("that mean remainder in our division how many it is ");
+console.log("and that is a easy way to understand how much remainer we have");
+console.log("if you want to know how much remainder you can use it");
+//10
+let myarray1=["Family",40,48,18,true ,undefined,null,]
+console.log('this works'+ myarray1);
+if (6/0 === 10/0){
+    console.log("yes")
+}else{
+    console.log("no")
+}
